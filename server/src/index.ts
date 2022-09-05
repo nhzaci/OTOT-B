b81dae47284037f690e992e6bc18a856477370c9
Zack Ng <ngzhicai98@gmail.com>
2022-09-05 21:49:50 +0800

    build(server): Amend build config

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,7 +1,7 @@
 import { app } from './app'
 import { connectDb } from './mongo'
 
-const port = process.env.PORT || 8000
+const port = process.env.PORT || 5000
 
 app.listen(port, () => {
   console.log(`App is live at port ${port}`)
